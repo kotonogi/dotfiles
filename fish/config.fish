b95c5ec4bb69a0c59bb368cda6be9f4399f836b6
waitold <chakkggm@gmail.com>
2023-10-16 08:32:37 +0900

    fix

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,7 @@ if status is-interactive
     set -g theme_date_format "+%Y-%m-%d %H:%M:%S" # 日付のフォーマットを変更(例： 2022-03-01 23:22:22)
     set -g fish_prompt_pwd_dir_length 0           # フォルダ名が省略されていたものを省略しなくする
     # Commands to run in interactive sessions can go here
+
+    source $HOME/.keychain/$HOST-fish
     eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
